b6956438148f0405c49852f02f9335bd476bae1c
lurts <47759111+lurts@users.noreply.github.com>
2024-11-05 21:29:40 +0100

    Add project files.

---
+++ P2A5_LinkedList.cpp
@@ -0,0 +1,12 @@
+﻿// P2A5_LinkedList.cpp : Defines the entry point for the application.
+//
+
+#include "P2A5_LinkedList.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
